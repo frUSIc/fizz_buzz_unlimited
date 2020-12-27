04d4e7de992b54b061447cfa94c6925140d41a2f
frUSIc <youshiz@hotmail.com>
2020-12-27 22:15:22 +1100

    Completed C# Implementation

---
+++ C#/fizzbuzz.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        private static string Divisible(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "fizzBuzz";
+            }
+            if (i % 3 == 0)
+            {
+                return "fizz";
+            }
+            if (i % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return i.ToString();
+        }
+
+        static void Main()
+        {
+            Console.Write("Please enter max number: ");
+            int maxNum = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= maxNum; i++)
+            {
+                Console.WriteLine(Divisible(i));
+            }
+        }
+    }
+}
